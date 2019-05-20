237ba9f1fba2184d76194497deb2f3093d2f4455
Udayan.Guha <messieur.udayanguha@gmail.com>
2019-05-20 15:50:41 +0530

    understanding try and except block

--- try_catch.py
+++ try_catch.py
@@ -1,3 +1,11 @@
-x = raw_input("Enter your age:")
+x = raw_input("Enter your age: ")
 try:
   age = int(x)
+except:
+    age = -1
+
+if age > 0 :
+    print( "Valid age entered having value: ")
+    print age
+elif age < 0:
+    print("Invalid age entered. Please re-enter")
