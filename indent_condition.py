dc4c9d33edd5db3cff05a57f425a4a1d322636b9
Udayan.Guha <messieur.udayanguha@gmail.com>
2019-05-20 13:14:17 +0530

    Python code to understand how indentation worsk

---
+++ indent_condition.py
@@ -0,0 +1,14 @@
+x=5
+print('Before if')
+print(x)
+if(x==5) :
+        print('start of 1st if')
+        print(x)
+        print("another statement inside 1st if")
+        print(x)
+print("Outside if")
+print(x)
+if(x==6):
+        print("Will this run inside 2nd if?")
+print("Did this run outside if?")
+print(x)
