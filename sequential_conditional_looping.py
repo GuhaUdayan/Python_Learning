02a8703a916cda2dc9e63e2ab10395c902e02cad
Udayan.Guha <messieur.udayanguha@gmail.com>
2019-05-20 13:38:21 +0530

    Understanding how looping, sequential and conditional code works

---
+++ sequential_conditional_looping.py
@@ -0,0 +1,13 @@
+x = 5
+print("initial value of x")
+print(x)
+if x > 3 :
+        print("bigger than 3")
+        print("inside the 1st if statement - conditional code")
+print("now out of if - sequential code")
+for i in range(5) :
+        print("inside a for loop")
+        if x > 2 :
+                print("Inside the 2nd if statement")
+        print("Done with i iteration", i)
+print("Completion of program")        
