407509e203f9317fb37a6ad7506a9048ede77e0f
Udayan.Guha <messieur.udayanguha@gmail.com>
2019-05-21 18:42:55 +0530

    pgm to count alphabets in user entered input

---
+++ alphabet_counter.py
@@ -0,0 +1,9 @@
+# count the number of an alphabet in a word entered by user
+x = raw_input("Enter any word or sentence :")
+y = raw_input("Enter the alphabet to be counted:")
+count = 0
+for i in x :
+    print("Alphabet is:", i)
+    if(i == y) :
+        count = count + 1
+print("Count of alphabet ", y, "is:", count)        
