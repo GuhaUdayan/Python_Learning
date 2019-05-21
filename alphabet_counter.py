b098fc60f61f74ea4edbf6143e875eccc3b0c39e
Udayan.Guha <messieur.udayanguha@gmail.com>
2019-05-21 18:43:54 +0530

    final

--- alphabet_counter.py
+++ alphabet_counter.py
@@ -3,7 +3,7 @@ x = raw_input("Enter any word or sentence :")
 y = raw_input("Enter the alphabet to be counted:")
 count = 0
 for i in x :
-    print("Alphabet is:", i)
+    #print("Alphabet is:", i)
     if(i == y) :
         count = count + 1
-print("Count of alphabet ", y, "is:", count)        
+print("Count of alphabet ", y, "is:", count)
