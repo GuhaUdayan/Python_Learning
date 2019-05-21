3a2927b78963b6867b13a2fa19799e53668cbc18
Udayan.Guha <messieur.udayanguha@gmail.com>
2019-05-21 18:25:26 +0530

    using for loop

--- printing_letter_word.py
+++ printing_letter_word.py
@@ -1,7 +1,12 @@
-x = raw_input("Enter any word or sentence :")
-i = 0
-while i < len(x) :
-    print("Alphabet printed is:", x[i], "at position :", i)
+# while loop methodology
+#x = raw_input("Enter any word or sentence :")
+#i = 0
+#while i < len(x) :
+#    print("Alphabet printed is:", x[i], "at position :", i)
     #print(x[i])
-    i = i+1
-    
+#    i = i+1
+
+#for loop menthodology
+x = raw_input("Enter any word or sentence :")
+for i in x :
+    print("Alphabet is:", i)
