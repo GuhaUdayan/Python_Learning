224a5c31edd6c00812a015cce4285d685191cbb6
Udayan.Guha <messieur.udayanguha@gmail.com>
2019-05-24 19:57:09 +0530

    alternate method

--- ten_most_comm_words.py
+++ ten_most_comm_words.py
@@ -1,5 +1,6 @@
 # read a file and calculate the 10 most common words and show them
 input_file = raw_input("enter a file:")
+if len(input_file) < 1 : input_file = "mbox_short.txt"
 handle = open(input_file)
 words = list()
 di = dict()
@@ -8,19 +9,24 @@ for line in handle :
     words = line.split()
     for word in words :
         di[word] = di.get(word, 0) + 1
-    print("dictionary is printed", di, "having count :", word )
+    #print("dictionary is printed", di, "having count :", word )
 
-print("Final dictionary", di)
+#print("Final dictionary", di)
+
+# methodology 1
+#lst = list()
+#for (k,v) in di.items() :
+#    newtup = (v,k)
+#    print("New tuple is printed", newtup)
+#    lst.append(newtup)
+#    print("New list is printed" , lst)
+
+#lst = sorted(lst, reverse = True)
+
+#for (k,v) in lst[:10] :
+#    print(v,k)
+# alternative methodology
 lst = list()
-for (k,v) in di.items() :
-    newtup = (v,k)
-    print("New tuple is printed", newtup)
-    lst.append(newtup)
-    print("New list is printed" , lst)
-
-lst = sorted(lst, reverse = True)
-
+lst = sorted ([ (v,k) for k,v in di.items()  ], reverse = True)
 for (k,v) in lst[:10] :
     print(v,k)
-
-    w5670002 user
