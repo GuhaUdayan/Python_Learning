ca5319aabd80862ef5df5d3f13d60967295c43c3
Udayan.Guha <messieur.udayanguha@gmail.com>
2019-05-31 13:33:09 +0530

    week 1 exercies

---
+++ Week_1_Exercies.py
@@ -0,0 +1,66 @@
+people = ['Dr. Christopher Brooks', 'Dr. Kevyn Collins-Thompson', 'Dr. VG Vinod Vydiswaran', 'Dr. Daniel Romero']
+
+def split_title_and_name(person):
+    title = person.split()[0]
+    lastname = person.split()[-1]
+    return '{} {}'.format(title, lastname)
+
+list(map(split_title_and_name, people))
+
+
+###################3
+
+
+people = ['Dr. Christopher Brooks', 'Dr. Kevyn Collins-Thompson', 'Dr. VG Vinod Vydiswaran', 'Dr. Daniel Romero']
+
+def split_title_and_name(person):
+    return person.split()[0] + ' ' + person.split()[-1]
+
+#option 1
+for person in people:
+    print(split_title_and_name(person) == (lambda x: x.split()[0] + ' ' + x.split()[-1])(person))
+
+#option 2
+list(map(split_title_and_name, people)) == list(map(lambda person: person.split()[0] + ' ' + person.split()[-1], people))
+
+
+
+============
+Many organizations have user ids which are constrained in some way. Imagine you work at an internet service provider and the user ids are all two letters followed by two numbers (e.g. aa49). Your task at such an organization might be to hold a record on the billing activity for each possible user.
+
+Write an initialization line as a single list comprehension which creates a list of all possible user ids. Assume the letters are all lower case.
+
+lowercase = 'abcdefghijklmnopqrstuvwxyz'
+digits = '0123456789'
+
+answer = [a+b+c+d for a in lowercase for b in lowercase for c in digits for d in digits]
+correct_answer == answer
+
+
+
+###############33
+
+
+old = np.array([[1, 1, 1],
+                [1, 1, 1]])
+
+new = old
+new[0, :2] = 0
+
+print(old)
+
+
+
+
+
+
+
+##################
+
+old = np.array([[1, 1, 1],
+                [1, 1, 1]])
+
+new = old.copy()
+new[:, 0] = 0
+
+print(old)
